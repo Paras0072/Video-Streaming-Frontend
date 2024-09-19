425695a841e05e64ca1d071fcb596b32fe839fce
Paras Thakur <parasthakur007241@gmail.com>
2024-09-19 22:52:07 +0530

    Part 4: Calling API' s of creating & joining room

--- video-frontend/src/api.js
+++ video-frontend/src/api.js
@@ -5,3 +5,50 @@ const API_URL = "http://localhost:5000";
 export const login = async (username, password) => {
   return axios.post(`${API_URL}/user/login`, { username, password });
 };
+export const register = async (username, password) => {
+  return axios.post(`${API_URL}/user/register`, {
+    username,
+    password,
+  });
+};
+export const roomCreation = async (token) => {
+  //return axios.post(`${API_URL}/room/create-room`,{},  { headers: {"x-auth-token":token}} );
+  try {
+    const response = await axios.post(
+      `${API_URL}/room/create-room`,
+      {},
+      {
+        headers: {
+          "x-auth-token": token,
+        },
+      }
+    );
+    return response.data; // Return the response data directly
+  } catch (error) {
+    console.error(
+      "Error creating room:",
+      error.response ? error.response.data : error.message
+    );
+    throw error; // Re-throw the error to handle it in the calling function
+  }
+};
+export const joining = async (token, roomId) => {
+  try {
+    const response = await axios.post(
+      `${API_URL}/room/join-room`,
+      { roomId },
+      {
+        headers: {
+          "x-auth-token": token,
+        },
+      }
+    );
+    return response.data; // Return the response data directly
+  } catch (error) {
+    console.error(
+      "Error joining room:",
+      error.response ? error.response.data : error.message
+    );
+    throw error; // Re-throw the error to handle it in the calling function
+  }
+};
